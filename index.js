c04f91e3ea9f125d950c19c2dff170963097e729
Yifan <datasciencewyfan@gmail.com>
2022-05-23 14:18:03 -0700

    commit tree 4cd16b1542e116137bd6c557f4570296293f38ab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const main = document.getElementById('main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+
+newHeader.setAttribute('Id' , 'victory')
+
+newHeader.innerHTML = 'YOUR-NAME is the champion'
